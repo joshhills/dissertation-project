50961423cfe4b15a537dbba39982fa4348957e96
Josh Hills <josh.hills@ymail.com>
2018-03-13 16:29:35 +0000

    Finished review metric investigations

--- source/dissertation/worker/worker.py
+++ source/dissertation/worker/worker.py
@@ -2,13 +2,14 @@
 
 import shared.database
 
-db = shared.database.Couchbase(host="couchbase://127.0.0.1")
+db = shared.database.Couchbase(host="couchbase://128.199.62.177")
 
 product_ids_results = db.run_query('select product_id from job', 'job')
 
-playtime_and_upvotes = []
+overall_results = []
 
 for row1 in product_ids_results:
+
     product_id = row1['product_id']
 
     print "Working on " + product_id
@@ -17,22 +18,23 @@ for row1 in product_ids_results:
     results = []
     while error:
         try:
-            results = db.run_query('select raw max([t.distinct_average_votes_up, {t.distinct_average_votes_up, t.author_total_playtime}])[1] from (select author_total_playtime, avg(distinct votes_up) as distinct_average_votes_up from review where product_id = "' + str(product_id) + '" group by author_total_playtime) t', 'job')
+            results = db.run_query('select raw max([votes_up, {product_id, votes_up, voted_up}])[1] from review where product_id = "' + str(product_id) + '"', 'job')
 
             for row in results:
                 results_object = {
-                    'author_total_playtime': row['author_total_playtime'],
-                    'distinct_average_votes_up': row['distinct_average_votes_up']
+                    'product_id': row['product_id'],
+                    'votes_up': row['votes_up'],
+                    'voted_up': row['voted_up']
                 }
 
             print results_object
             print '=-=-=-=-=-=-=-=-=-=-=-='
 
-            playtime_and_upvotes.append(results_object)
+            overall_results.append(results_object)
 
             error = False
         except Exception, e:
             print "Timed out on {0}, trying again.".format(product_id)
 
 print "Finally"
-print playtime_and_upvotes
+print overall_results
